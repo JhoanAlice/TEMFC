1e68f667b46faa860dd95b92ecdf273eb8d30030
Jhoan Alice Franco <jhoanjfranco@icloud.com>
2025-03-22 23:33:23 -0300

    Initial Commit

---
+++ TEMFC/TEMFCApp.swift
@@ -0,0 +1,32 @@
+//
+//  TEMFCApp.swift
+//  TEMFC
+//
+//  Created by Jhoan Franco on 3/22/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TEMFCApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TEMFCTests/TEMFCTests.swift
@@ -0,0 +1,17 @@
+//
+//  TEMFCTests.swift
+//  TEMFCTests
+//
+//  Created by Jhoan Franco on 3/22/25.
+//
+
+import Testing
+@testable import TEMFC
+
+struct TEMFCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
